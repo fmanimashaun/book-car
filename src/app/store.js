8222ebf8d337d5220a2f2311ac708c5c15f0804b
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-02-19 21:41:51 +0300

    Apply redux persist library to persist data for application

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import AppDataSlice from './redux/AppDataSlice';
 import carDetailsSlice from './redux/carDetailsSlice';
+import { authReducer } from './redux/auth/authSlice';
+
+const rootReducer = combineReducers({
+  appData: AppDataSlice,
+  carDetails: carDetailsSlice,
+  auth: authReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  whitelist: ['auth'],
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    appData: AppDataSlice,
-    carDetails: carDetailsSlice,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export default store;
