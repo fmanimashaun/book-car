551f4c5f01fe469c9b965f38aef1fc6aea8ec337
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-02-19 23:53:29 +0430

    Add the DeletCar component

--- src/pages/DeleteCars.jsx
+++ src/pages/DeleteCars.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
-const DeleteCars = () => (
-  <div>
-    <h1>Delete Cars</h1>
-    <p>List of available cars that can be deleted</p>
-  </div>
-);
+const DeleteCars = () => {
+  let cars = useSelector((store) => store.appData.appData.cars);
+  cars = [...cars];
+  if (cars) cars.sort((a, b) => a.id - b.id);
+
+  
+  return (
+    <ul>
+      {cars.map((car) => (
+        <li key={car.id}>
+          <p>{car.id}</p>
+          <p>{car.name}</p>
+          <button type="button" onClick={() => handleDelete(car.id)}>
+            Delete
+          </button>
+          <br />
+          <br />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default DeleteCars;
