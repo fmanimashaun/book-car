9897584b32f7e3d9ee7f78ad11ed84c6e3ec0af2
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-02-20 10:34:46 +0300

    Make changes to middlewares to avoid logging errors

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import AppDataSlice from './redux/AppDataSlice';
 import carDetailsSlice from './redux/carDetailsSlice';
 import { authReducer } from './redux/auth/authSlice';
@@ -21,6 +30,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 export const persistor = persistStore(store);
