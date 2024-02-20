8757b88ed3f01938853929e638fd132ffdf9fc85
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-02-20 21:58:35 +0430

    Add deleteCar function to the AppDataSlice

--- src/pages/DeleteCars.jsx
+++ src/pages/DeleteCars.jsx
@@ -1,10 +1,48 @@
+import { deleteCar } from 'app/redux/AppDataSlice';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const DeleteCars = () => (
-  <div>
-    <h1>Delete Cars</h1>
-    <p>List of available cars that can be deleted</p>
-  </div>
-);
+const DeleteCars = () => {
+  const cars = [...useSelector((store) => store.appData.appData.cars)];
+  const token = useSelector((store) => store.auth.token);
+  const dispatch = useDispatch();
+  if (cars.length) {
+    cars.sort((a, b) => a.id - b.id);
+  }
+
+  const handleDelete = (carId) => {
+    dispatch(deleteCar({ carId, token }));
+    console.log(token);
+  };
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Number</th>
+          <th>Car Name</th>
+          <th>Operation</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cars.map((car) => (
+          <tr key={car.id}>
+            <td>{car.id}</td>
+            <td>{car.name}</td>
+            <td>
+              <button
+                className="btn btn-danger text-success"
+                type="button"
+                onClick={() => handleDelete(car.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default DeleteCars;
