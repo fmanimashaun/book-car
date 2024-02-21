d91e8de4d442aa88e09b64f445f3371223dc4919
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-02-21 20:06:55 -0300

    style form

--- src/pages/AddReserve.jsx
+++ src/pages/AddReserve.jsx
@@ -52,41 +52,32 @@ const Reserve = () => {
 
   return (
     <div
-      className="bg-opacity-70 rounded-lg h-full bg-center bg-no-repeat p-10 text-white text-center "
+      className="bg-opacity-70 rounded-lg h-full bg-center bg-no-repeat p-40 text-white text-center flex-col justify-center"
       style={{ backgroundImage: `linear-gradient(rgba(151, 191, 15, 0.9), rgba(151, 191, 15, 0.9)), url(${carCollage})` }}
     >
-      <h2 className="bold text-3xl font-black tracking-widest">BOOK A CAR TEST-RIDE</h2>
-      <form onSubmit={handleSubmit}>
+      <h2 className="bold text-3xl font-bold tracking-widest border-b mx-auto max-w-md pb-4">BOOK A CAR TEST-RIDE</h2>
+      <p className="text-center py-4">
+        There are over 20 different car models to choose from.
+        Our ever-expanding selection offers various types of cars, from sports to casual.
+        We have showrooms all over the US with test-drive facilities.
+        If you wish to rent one of our models please use the selector below.
+      </p>
+      <form onSubmit={handleSubmit} className="pt-5 flex justify-around font-medium">
         {error && <p>{error}</p>}
-        <div>
-          <label htmlFor="carId">
-            Select Car:
-            <select id="carId" name="carId" value={carId} onChange={(e) => setCarId(e.target.value)}>
-              <option value="">Select a car</option>
-              {cars.map((car) => (
-                <option key={car.id} value={car.id}>{car.name}</option>
-              ))}
-            </select>
-          </label>
-        </div>
-        <div>
-          <label htmlFor="city">
-            Select City:
-            <select id="city" value={city} onChange={(e) => setCity(e.target.value)}>
-              <option value="">Select a city</option>
-              {cities.map((city) => (
-                <option key={city.id} value={city.id}>{city.name}</option>
-              ))}
-            </select>
-          </label>
-        </div>
-        <div>
-          <label htmlFor="date">
-            Date:
-            <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} />
-          </label>
-        </div>
-        <button type="submit">Submit Reservation</button>
+        <select className="bg-light-green p-4 rounded-full border-2 border-white focus:outline-none" id="carId" name="carId" value={carId} onChange={(e) => setCarId(e.target.value)}>
+          <option value="">Select a car</option>
+          {cars.map((car) => (
+            <option key={car.id} value={car.id}>{car.name}</option>
+          ))}
+        </select>
+        <select className="bg-light-green p-4 rounded-full border-2 border-white focus:outline-none" id="city" value={city} onChange={(e) => setCity(e.target.value)}>
+          <option value="">Select a city</option>
+          {cities.map((city) => (
+            <option key={city.id} value={city.id}>{city.name}</option>
+          ))}
+        </select>
+        <input className="bg-light-green p-4 rounded-full border-2 border-white focus:outline-none" type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} />
+        <button className="bg-white p-4 rounded-full border border-white text-light-green" type="submit">Book now</button>
         {successMessage && <p>{successMessage}</p>}
       </form>
     </div>
