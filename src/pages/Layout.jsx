3820ffb89889992867637e3720bf6d876e910c64
Esteban <cepalacios@outlook.com>
2024-02-21 20:40:50 -0500

    Implement responsiveness in Layout for sidebar and CarList for carrousel components

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAppData } from 'app/redux/AppDataSlice';
@@ -7,6 +7,9 @@ import Sidebar from 'components/Sidebar';
 const Layout = () => {
   const dispatch = useDispatch();
   const { loading, error } = useSelector((state) => state.appData);
+
+  // State to manage sidebar visibility
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   useEffect(() => {
     dispatch(fetchAppData());
@@ -31,10 +34,25 @@ const Layout = () => {
 
   return (
     <>
-      <Sidebar />
-      <main className="flex-1 text-black p-4 overflow-auto">
-        {renderContent()}
-      </main>
+      <div className="md:hidden">
+        {' '}
+        {/* Show only on small screens */}
+        <button
+          type="button"
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="fixed top-0 left-0 z-50 p-4 focus:outline-none"
+        >
+          {isSidebarOpen ? 'Close' : 'Open'}
+          <br />
+          Menu
+        </button>
+      </div>
+      <div className={`md:block ${isSidebarOpen ? 'block' : 'hidden'}`}>
+        {' '}
+        {/* Show on medium and larger screens if isSidebarOpen is true */}
+        <Sidebar />
+      </div>
+      <main className="flex-1 text-black p-4 overflow-auto">{renderContent()}</main>
     </>
   );
 };
