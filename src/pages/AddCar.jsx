cc9ffdb6c337cd6eca4766687692b06c68bf5fae
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-02-22 09:48:59 +0430

    fetche engyin types from the store

--- src/pages/AddCar.jsx
+++ src/pages/AddCar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function AddCar() {
+  const engineTypes = useSelector((store) => store.appData.appData.engine_type);
   return <div>AddCar</div>;
 }
