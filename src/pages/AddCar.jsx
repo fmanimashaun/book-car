65a720e446ff354713265defd1130a065001a6cc
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-02-22 09:49:55 +0430

    Create the formData state

--- src/pages/AddCar.jsx
+++ src/pages/AddCar.jsx
@@ -3,5 +3,21 @@ import { useSelector } from 'react-redux';
 
 export default function AddCar() {
   const engineTypes = useSelector((store) => store.appData.appData.engine_type);
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    image_url: '',
+    engine_type_id: '',
+    horsepower: null,
+    torque: null,
+    fuel_economy: '',
+    range: '',
+    seating_capacity: '',
+    cargo_space: '',
+    infotainment_system: '',
+    safety_rating: '',
+    tech_features: '',
+    special_features: '',
+  });
   return <div>AddCar</div>;
 }
