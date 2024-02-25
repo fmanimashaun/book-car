9100a03f50d135b7846d3214ef6d03b8b3b2b922
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-25 09:50:57 +0100

    Add the reservation list component

---
+++ src/components/ReservationList.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ReservationList = ({ reservations }) => (
+  <div className="overflow-x-auto max-w-[500px]">
+    <table className="w-full table-auto border-collapse border border-black">
+      <thead>
+        <tr>
+          <th className="px-2 py-2 font-bold text-center whitespace-nowrap border border-black">S/N</th>
+          <th className="px-2 py-2 font-bold text-center border border-black">RERSERVATIONS</th>
+        </tr>
+      </thead>
+      <tbody>
+        {reservations.map((reservation, index) => (
+          <tr key={reservation.id} className="bg-gray-100">
+            <td className="px-2 py-2 text-center border border-black">{index + 1}</td>
+            <td className="px-2 border border-black">
+              <div className="flex flex-col md:flex-row items-center space-x-2">
+                <img className="w-24 h-24 object-contain rounded-lg" src={reservation.car.image_url} alt={reservation.car.name} />
+                <div className="flex-1">
+                  <h2 className="text-xl font-semibold">{reservation.car.name}</h2>
+                  <p className="text-sm text-gray-500">{reservation.car.description}</p>
+                  <p className="text-sm">
+                    <span className="font-semibold">City:</span>
+                    {' '}
+                    {reservation.city.name}
+                  </p>
+                  <p className="text-sm">
+                    <span className="font-semibold">Reservation Date:</span>
+                    {' '}
+                    {reservation.date}
+                  </p>
+                </div>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
+
+ReservationList.propTypes = {
+  reservations: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      date: PropTypes.string.isRequired,
+      user_id: PropTypes.number.isRequired,
+      user: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+      city: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+      car: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        image_url: PropTypes.string.isRequired,
+      }).isRequired,
+    }),
+  ).isRequired,
+};
+
+export default ReservationList;
--- src/pages/MyResevation.jsx
+++ src/pages/MyResevation.jsx
@@ -1,8 +1,51 @@
-const MyResevation = () => (
-  <div>
-    <h1>My Resevation</h1>
-    <p>The list of reservation</p>
-  </div>
-);
+import ReservationList from 'components/ReservationList';
+
+const MyResevation = () => {
+  const data = [
+    {
+      id: 1,
+      date: '2024-02-27',
+      user_id: 1,
+      user: {
+        id: 1,
+        name: 'Admin User',
+      },
+      city: {
+        id: 2,
+        name: 'Los Angeles',
+      },
+      car: {
+        id: 2,
+        name: 'Honda Accord',
+        description: 'Sporty yet sensible, the Accord offers enjoyable handling, ample passenger room, and excellent gas mileage.',
+        image_url: 'http://localhost:4000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6MiwicHVyIjoiYmxvYl9pZCJ9fQ==--6206a9ad28842c3722f93eefb58dbad50fcbfea4/Honda-Accord.png',
+      },
+    },
+    {
+      id: 2,
+      date: '2024-02-28',
+      user_id: 1,
+      user: {
+        id: 1,
+        name: 'Admin User',
+      },
+      city: {
+        id: 2,
+        name: 'Los Angeles',
+      },
+      car: {
+        id: 2,
+        name: 'Honda Accord',
+        description: 'Sporty yet sensible, the Accord offers enjoyable handling, ample passenger room, and excellent gas mileage.',
+        image_url: 'http://localhost:4000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6MiwicHVyIjoiYmxvYl9pZCJ9fQ==--6206a9ad28842c3722f93eefb58dbad50fcbfea4/Honda-Accord.png',
+      },
+    },
+  ];
+  return (
+    <div>
+      <ReservationList reservations={data} />
+    </div>
+  );
+};
 
 export default MyResevation;
